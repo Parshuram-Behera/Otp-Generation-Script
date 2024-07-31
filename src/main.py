0550c63c9d3ef4490fe6e3c13562f7a3cbd2989d
Parshuramb7 <parshurambehera7735@gmail.com>
2024-07-31 23:26:17 +0530

    2nd file test

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from pytubefix import YouTube
 
 def getDownloadUrls(video_url):
     try:
-        yt = YouTube(video_url,use_oauth=false, allow_oauth_cache=false)
+        yt = YouTube(video_url,use_oauth= false)
         streams = yt.streams.order_by('resolution')
         download_list = []
 
